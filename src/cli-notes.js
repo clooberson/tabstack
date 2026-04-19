4cfcd23c6f36ae48f50fc949d0c82fc620a4fd48
Clooby <clooberson@users.noreply.github.com>
2026-04-20 03:39:19 +0800

    add module-level comment to cli-notes.js

--- src/cli-notes.js
+++ src/cli-notes.js
@@ -1,2 +1,6 @@
+/**
+ * CLI notes module - exposes notes command registration for the tabstack CLI.
+ * Delegates implementation to cli-notes-handler.
+ */
 const { registerNotesCommand } = require('./cli-notes-handler');
 module.exports = { registerNotesCommand };
